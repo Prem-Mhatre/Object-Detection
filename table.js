a47abc8709a29f0558edee5a1fe1567ca77bdbfe
Prem Mhatre <83350182+Prem-Mhatre@users.noreply.github.com>
2021-11-20 13:27:07 +0530

    Add files via upload

---
+++ table.js
@@ -0,0 +1,32 @@
+img = "";
+status = "";
+function preload(){
+    img = loadImage("table.png");
+}
+
+function setup(){
+    canvas = createCanvas(640, 420);
+    canvas.center();
+
+    document.getElementById("status").innerHTML = "Status: Detecting Objects"; 
+    object_detector = ml5.objectDetector("cocossd", modelLoaded);
+}
+
+function modelLoaded(){
+    console.log("cocossd model is loaded");
+    status = true;
+    object_detector.detect(img, gotResult);
+}
+
+function gotResult(error, results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+    }
+}
+
+function draw(){
+    image(img, 0, 0, 640, 420);
+}
